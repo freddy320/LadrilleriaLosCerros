3aaac2bfb4898e8786df583d56576e9842a200f1
Leonardo Pastrana <leopas2004@gmail.com>
2024-10-05 14:46:31 -0500

    modificaciones al estilo de 'Panel'

--- frontend/ladrilleriaLosCerros/src/components/Panel.jsx
+++ frontend/ladrilleriaLosCerros/src/components/Panel.jsx
@@ -4,46 +4,46 @@ import { FaPiggyBank, FaShoppingCart } from "react-icons/fa";
 export default function Panel() {
     return (
         <div>
-           <section className='resume-custom'>
-          <div className='container-custom'>
-            <div className='icon-container-custom' style={{ backgroundColor: '#E3FCEE', color: '#1E9E5E' }}>
-              <RiMoneyDollarCircleLine />
-            </div>
-            <div>
-              <h5>Ganancias</h5>
-              <h3>$1000</h3>
-              <h6>$50 al mes</h6>
-            </div>
-          </div>
-          <div className='container-custom'>
-            <div className='icon-container-custom' style={{ backgroundColor: '#CFF4FE', color: '#326C92' }}>
-              <FaPiggyBank />
-            </div>
-            <div>
-              <h5>Ganancias</h5>
-              <h3>$1000</h3>
-              <h6>$50 al mes</h6>
-            </div>
-          </div>
-          <div className='container-custom'>
-            <div className='icon-container-custom' style={{ backgroundColor: '#FECAF2', color: '#E76899' }}>
-              <FaShoppingCart />
-            </div>
-            <div>
-              <h5>Ganancias</h5>
-              <h3>$1000</h3>
-              <h6>$50 al mes</h6>
-            </div>
-          </div>
-        </section>
+            <section className='resume-custom'>
+                <div className='container-custom'>
+                    <div className='icon-container-custom' style={{ backgroundColor: '#E3FCEE', color: '#1E9E5E' }}>
+                        <RiMoneyDollarCircleLine />
+                    </div>
+                    <div>
+                        <h5 className="text-base font-semibold">Ganancias</h5>
+                        <h3 className="text-xl font-bold ">$1000</h3>
+                        <h6 className="text-sm">$50 en el útlimo mes</h6>
+                    </div>
+                </div>
+                <div className='container-custom'>
+                    <div className='icon-container-custom' style={{ backgroundColor: '#CFF4FE', color: '#326C92' }}>
+                        <FaPiggyBank />
+                    </div>
+                    <div>
+                    <h5 className="text-base font-semibold">Ganancias</h5>
+                        <h3 className="text-xl font-bold ">$1000</h3>
+                        <h6 className="text-sm">$50 en el útlimo mes</h6>
+                    </div>
+                </div>
+                <div className='container-custom'>
+                    <div className='icon-container-custom' style={{ backgroundColor: '#FECAF2', color: '#E76899' }}>
+                        <FaShoppingCart />
+                    </div>
+                    <div>
+                    <h5 className="text-base font-semibold">Ganancias</h5>
+                        <h3 className="text-xl font-bold ">$1000</h3>
+                        <h6 className="text-sm">$50 en el útlimo mes</h6>
+                    </div>
+                </div>
+            </section>
 
-        <section className='details-custom'>
-          <div className='grafict-custom' id='grafict'>
-            Informe de ventas por año
-          </div>
-          <div className='clients-custom'>Porcentaje de clientes</div>
-          <div className='products-custom'>Historial de productos Vendidos</div>
-        </section>
+            <section className='details-custom'>
+                <div className='grafict-custom' id='grafict'>
+                    Informe de ventas por año
+                </div>
+                <div className='clients-custom'>Porcentaje de clientes</div>
+                <div className='products-custom'>Historial de productos Vendidos</div>
+            </section>
         </div>
     )
 }
