a46dfbe81d2374f31db3109396b13384df693284
Leonardo Pastrana <leopas2004@gmail.com>
2024-10-05 14:22:10 -0500

    Integracion de Tailwindcss al proyecto

---
+++ frontend/ladrilleriaLosCerros/src/components/Panel.jsx
@@ -0,0 +1,49 @@
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { FaPiggyBank, FaShoppingCart } from "react-icons/fa";
+
+export default function Panel() {
+    return (
+        <div>
+           <section className='resume-custom'>
+          <div className='container-custom'>
+            <div className='icon-container-custom' style={{ backgroundColor: '#E3FCEE', color: '#1E9E5E' }}>
+              <RiMoneyDollarCircleLine />
+            </div>
+            <div>
+              <h5>Ganancias</h5>
+              <h3>$1000</h3>
+              <h6>$50 al mes</h6>
+            </div>
+          </div>
+          <div className='container-custom'>
+            <div className='icon-container-custom' style={{ backgroundColor: '#CFF4FE', color: '#326C92' }}>
+              <FaPiggyBank />
+            </div>
+            <div>
+              <h5>Ganancias</h5>
+              <h3>$1000</h3>
+              <h6>$50 al mes</h6>
+            </div>
+          </div>
+          <div className='container-custom'>
+            <div className='icon-container-custom' style={{ backgroundColor: '#FECAF2', color: '#E76899' }}>
+              <FaShoppingCart />
+            </div>
+            <div>
+              <h5>Ganancias</h5>
+              <h3>$1000</h3>
+              <h6>$50 al mes</h6>
+            </div>
+          </div>
+        </section>
+
+        <section className='details-custom'>
+          <div className='grafict-custom' id='grafict'>
+            Informe de ventas por año
+          </div>
+          <div className='clients-custom'>Porcentaje de clientes</div>
+          <div className='products-custom'>Historial de productos Vendidos</div>
+        </section>
+        </div>
+    )
+}
--- frontend/ladrilleriaLosCerros/src/components/menu.jsx
+++ frontend/ladrilleriaLosCerros/src/components/menu.jsx
@@ -1,15 +1,13 @@
-import '../style/App.css'
-import losCerros from '../utils/pintures/logo-los-cerros.webp'
+import '../style/App.css';
+import losCerros from '../utils/pintures/logo-los-cerros.webp';
 import { TfiBlackboard } from "react-icons/tfi";
 import { AiFillProduct } from "react-icons/ai";
 import { IoPersonOutline } from "react-icons/io5";
 import { MdPointOfSale } from "react-icons/md";
 import { CiMoneyCheck1 } from "react-icons/ci";
-import { RiMoneyDollarCircleLine } from "react-icons/ri";
-import { FaPiggyBank } from "react-icons/fa";
-import { FaShoppingCart } from "react-icons/fa";
 
-import Chart  from 'chart.js/auto'
+import Chart from 'chart.js/auto';
+import Panel from './Panel';
 
 const data = [
   { year: 2010, count: 10 },
@@ -36,77 +34,38 @@ new Chart(
     }
   }
 );
+
 const Item = ({ children }) => {
   return (
-    <div className="item">{children}</div>
+    <div className="item-custom">{children}</div>
   );
-}
+};
 
 export function Menu() {
   return (
-    <main className="home">
-      <aside className="aside">
-        <header className="header">
-          <div className="pintureContainer">
-            <img className="pinture" src={losCerros} alt="logo" />
+    <main className="home-custom">
+      <aside className="aside-custom">
+        <header className="header-custom">
+          <div className="pinture-container-custom">
+            <img className="pinture-custom" src={losCerros} alt="logo" />
           </div>
           <nav>
-            <ul className="nav">
-              <Item><TfiBlackboard className='icon' />Panel</Item>
-              <Item><AiFillProduct className='icon' />Productos</Item>
-              <Item><IoPersonOutline className='icon' />Clientes</Item>
-              <Item><MdPointOfSale className='icon' />Ingreso</Item>
-              <Item><CiMoneyCheck1 className='icon' />Cartera</Item>
+            <ul className="nav-custom">
+              <Item><TfiBlackboard className='icon-custom' />Panel</Item>
+              <Item><AiFillProduct className='icon-custom' />Productos</Item>
+              <Item><IoPersonOutline className='icon-custom' />Clientes</Item>
+              <Item><MdPointOfSale className='icon-custom' />Ingreso</Item>
+              <Item><CiMoneyCheck1 className='icon-custom' />Cartera</Item>
             </ul>
           </nav>
         </header>
-        <footer className='footer'>
-          <div className='avatarContainer'></div>
+        <footer className='footer-custom'>
+          <div className='avatar-container-custom'></div>
           <div className='text'>Username</div>
         </footer>
       </aside>
-      <section className='content'>
-       
-        <section className='resume'>
-          <div className='container'>
-            <div className='iconContainerMoney'style={{ backgroundColor: '#E3FCEE',color:'#1E9E5E'}} >
-              <RiMoneyDollarCircleLine />
-            </div>
-            <div>
-              <h5>Ganancias</h5>
-              <h3>$1000</h3>
-              <h6>$50 al mes</h6>
-            </div>
-          </div>
-          <div className='container'>
-            <div className='iconContainerBank' style={{ backgroundColor: '#CFF4FE',color:'#326C92' }}>
-              <FaPiggyBank />
-            </div>
-            <div>
-              <h5>Ganancias</h5>
-              <h3>$1000</h3>
-              <h6>$50 al mes</h6>
-            </div>
-          </div>
-          <div className='container'>
-            <div className='iconContainerShop' style={{ backgroundColor: '#FECAF2',color:'#E76899'}}>
-              <FaShoppingCart />
-            </div>
-            <div>
-              <h5>Ganancias</h5>
-              <h3>$1000</h3>
-              <h6>$50 al mes</h6>
-            </div>
-          </div>
-        </section>
-
-        <section className='details'>
-          <div className='grafict' id='grafict'>
-            Informe de ventas por año
-          </div>
-          <div className='clients'>Porcentaje de clientes</div>
-          <div className='products'>Historial de productos Vendidos</div>
-        </section>
+      <section className='content-custom'>
+       <Panel />
       </section>
     </main>
   );
