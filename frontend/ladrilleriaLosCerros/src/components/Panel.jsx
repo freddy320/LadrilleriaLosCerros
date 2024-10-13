081f5f068a2b47cf1a2ab06026800f459edfeac2
Darkin03 <esnaiderortega@unisinu.edu.co>
2024-10-12 19:38:56 -0500

    asss

--- frontend/ladrilleriaLosCerros/src/components/Panel.jsx
+++ frontend/ladrilleriaLosCerros/src/components/Panel.jsx
@@ -6,6 +6,18 @@ import Filtros from "./panel/Filtros";
 ChartJs.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend, Title);
 
 
+
+const CardProduct = ({nombre,Unidades,totalRecaudo})=>{
+    return(
+        <div className="bg-contrast p-4 rounded-xl shadow-md"> 
+            <h3 className="font-bold text-lg">{nombre}</h3>
+            <p className="text-sm opacity-65">{Unidades}</p>
+            <h6 className="text-lg font-bold">{totalRecaudo}</h6>      
+        </div>
+    )
+}
+
+
 const CardContainer = ({ children }) => {
     return (
         <div className="grid grid-cols-3 grid-cols-gridGraficts h-28">
@@ -58,19 +70,17 @@ const ItemPanel = ({ title, resultText, result, icon }) => {
 
 export default function Panel() {
     return (
-        <div className="flex h-full gap-4">
-            {/* Middle section (KPIs and Charts) */}
+        <main className="flex h-full gap-4">
             <div className="flex flex-col gap-4 w-4/5">
-                {/* KPIs section */}
                 <section className='flex gap-8 justify-center bg-contrast p-4 rounded-xl shadow-lg'>
                     <ItemPanel title="Ganancias" resultText="$50 en el último mes" result="$ 1000" icon={<CiBadgeDollar className="w-12 h-12" />} />
                     <ItemPanel title="Promedio" resultText="+ 1.5 de ventas en el último mes" result="50.5" icon={<CiCalculator1 className="w-12 h-12" />} />
                     <ItemPanel title="Ventas" resultText="+ 50 ventas en el último mes" result="1000" icon={<CiShoppingCart className="w-12 h-12" />} />
                 </section>
 
-                {/* Charts section */}
+
+                <section className="grid grid-cols-gridGraficts grid-cols-1 gap-2">
                 <section className='grid grid-cols-2 gap-2'>
-                    {/* Sales Report Chart */}
                     <div className='bg-contrast px-5 py-2 rounded-xl shadow-lg'>
                         <header className="flex justify-between items-baseline">
                             <h2 className="font-extrabold text-xl">Informe general de ventas</h2>
@@ -80,20 +90,29 @@ export default function Panel() {
                         </div>
                     </div>
 
-                    {/* Clients Chart */}
                     <div className='bg-contrast px-5 py-2 rounded-xl shadow-lg'>
                         <h2 className="font-semibold text-xl">Clientes</h2>
-                        {/* Additional chart or content for Clients */}
                     </div>
                 </section>
+                <section className="h-full bg-contrast px-5 py-2 rounded-xl shadow-lg">
+                    <header><h2 className="font-extrabold text-xl">Productos vendidos</h2></header>
+                    <div className="grid gap-2 overflow-y-scroll py-1">
+                       
+                            <CardProduct nombre="Ladrillo" Unidades="1000 unidades" totalRecaudo="$ 1000" />
+                            <CardProduct nombre="Cemento" Unidades="1000 unidades" totalRecaudo="$ 1000" />
+                            <CardProduct nombre="Arena" Unidades="1000 unidades" totalRecaudo="$ 1000" />
+                    </div>
+                </section>
+                </section>
+                
             </div>
 
-            {/* Right side (Filters) */}
+            
             <section className="w-1/5 bg-contrast p-4 rounded-xl shadow-lg">
             <h3 className="font-bold text-base">Filtros</h3>
                 <Filtros />
             </section>
-        </div>
+        </main>
 
 
     )
